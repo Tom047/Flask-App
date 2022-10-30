d436074a57106754238b17f2a42b70650cea4956
Tom047 <99247067+Tom047@users.noreply.github.com>
2022-10-30 23:46:01 +0600

    Update project.py

--- src/project.py
+++ src/project.py
@@ -1,8 +1,13 @@
 import collections
+import bcrypt
 from time import sleep
 from json2html import * 
 import requests, psycopg2, json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
 
 # Connection to DB
 conn = psycopg2.connect(database="nft_test", user = "postgres", password = "PASSWORD", host = "127.0.0.1", port = "5432")
@@ -15,6 +20,18 @@ headers = {
 }
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Your_secret_key'
+bootstrap = Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=4, max=15)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=4, max=15)])
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -104,6 +121,58 @@ def index():
             return render_template('result.html', table = json2html.convert(json = response))
 
     return render_template('index.html')  
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        remember = form.remember.data
+
+        # Checking if the Users table exists in our database
+        cur = conn.cursor()
+        cur.execute("select * from information_schema.tables where table_name='users'")
+        check1 = bool(cur.rowcount)
+        check2 = False
+        # Checking if our table has this particular user
+        if check1:
+            cur.execute("SELECT name FROM users WHERE name = %s", (username,))
+            check2 = (cur.fetchone() is not None)   
+
+        # If this user is already in our table
+        if check2:
+            cur.execute('''SELECT * FROM users WHERE name=%s''', (username,))
+            user = cur.fetchone()
+
+            salt = bytes(user[2], 'utf-8')
+            if bcrypt.checkpw(password.encode('utf-8'), salt):
+                return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data      
+
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+        with conn.cursor() as cur:
+            cur.execute(""" CREATE TABLE IF NOT EXISTS users(
+                name text, email text, pass text, PRIMARY KEY (name)) """)
+            query_sql = """ INSERT INTO users(name, email, pass) VALUES(%s, %s, %s) """
+            cur.execute(query_sql, (username, email, hashed_password,))
+            conn.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('signup.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)

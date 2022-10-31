f584fc55d02895ffc5c235d736ec23f13e08a2e0
Makkkke <94453183+Makkkke@users.noreply.github.com>
2022-10-31 23:38:06 +0600

    Update project.py

--- src/project.py
+++ src/project.py
@@ -4,52 +4,92 @@ from time import sleep
 from json2html import * 
 import requests, psycopg2, json
 from flask import Flask, render_template, request, redirect, url_for
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, ValidationError
 from wtforms.validators import InputRequired, Email, Length
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager, UserMixin, login_required, login_user, login_remembered, logout_user
+from flask_sqlalchemy import SQLAlchemy
 
 # Connection to DB
-conn = psycopg2.connect(database="nft_test", user = "postgres", password = "PASSWORD", host = "127.0.0.1", port = "5432")
+
+conn = psycopg2.connect(database="nft_test", user = "postgres", password = "Murad2001", host = "127.0.0.1", port = "5432")
+cur = conn.cursor()
+cur.execute(""" CREATE TABLE IF NOT EXISTS users(name text, email text, password text, PRIMARY KEY (name)) """)
+cur.close()
 
 url = "https://solana-gateway.moralis.io/nft/mainnet/{}/metadata"
 
 headers = {
     "accept": "application/json",
-    "X-API-Key": "YOUR OWN API-KEY"
+    "X-API-Key": "ji6Went1H9iyAf99QlBqID362KCuSwloYwVgyrw2IkTFbYQjK7ARYzVDcEMbXgNe"
 }
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Your_secret_key'
+app.config['SECRET_KEY'] = 'Mysecretkey'
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:Murad2001@localhost/nft_test"
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class Users(UserMixin, db.Model):
+    name = db.Column(db.Text, primary_key=True)
+    email = db.Column(db.Text)
+    password = db.Column(db.Text)
+
+    def get_id(self):
+        return (self.name)
+
+@login_manager.user_loader
+def load_user(name):
+    return Users.query.filter_by(name=name).first()
 
 class LoginForm(FlaskForm):
+    def validate_password(form, password):
+        user = Users.query.filter_by(name=form.username.data).first()
+        if user:
+            salt = bytes(user.password, 'utf-8')
+            if not bcrypt.checkpw(password.data.encode('utf-8'), salt):
+                raise ValidationError('Password is incorrect')
+
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=4, max=15)])
     remember = BooleanField('remember me')
 
 class RegisterForm(FlaskForm):
+    def validate_username(form, username):
+        user = Users.query.filter_by(name=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose another.')
+
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=4, max=15)])
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    return render_template('index.html')
+
+@app.route("/search", methods=['GET', 'POST'])
+@login_required
+def search():
     if request.method == 'POST':
         address = request.form.get('address')
 
         # Checking if the nft table exists in our database
         cur = conn.cursor()
         cur.execute("select * from information_schema.tables where table_name='nft_table'")
-        check1 = bool(cur.rowcount)
-        check2 = False
+        nft_table_exists = bool(cur.rowcount)
+        nft_exist = False
         # Checking if our table has this particular address
-        if check1:
+        if nft_table_exists:
             cur.execute("SELECT mint FROM nft_table WHERE mint = %s", (address,))
-            check2 = (cur.fetchone() is not None)   
+            nft_exist = (cur.fetchone() is not None)   
 
         # If this address is already in our table, let's try to display it
-        if check2:
+        if nft_exist:
 
             # The code below converts our record from the database to JSON, similar to what we get from Moralis API
             cur.execute('''SELECT * FROM nft_table LEFT JOIN multiplex_table USING(metaplex_id) 
@@ -120,7 +160,7 @@ def index():
             
             return render_template('result.html', table = json2html.convert(json = response))
 
-    return render_template('index.html')  
+    return render_template('search.html')  
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -131,24 +171,12 @@ def login():
         password = form.password.data
         remember = form.remember.data
 
-        # Checking if the Users table exists in our database
-        cur = conn.cursor()
-        cur.execute("select * from information_schema.tables where table_name='users'")
-        check1 = bool(cur.rowcount)
-        check2 = False
-        # Checking if our table has this particular user
-        if check1:
-            cur.execute("SELECT name FROM users WHERE name = %s", (username,))
-            check2 = (cur.fetchone() is not None)   
-
-        # If this user is already in our table
-        if check2:
-            cur.execute('''SELECT * FROM users WHERE name=%s''', (username,))
-            user = cur.fetchone()
-
-            salt = bytes(user[2], 'utf-8')
+        user = Users.query.filter_by(name=username).first()
+        if user:
+            salt = bytes(user.password, 'utf-8')
             if bcrypt.checkpw(password.encode('utf-8'), salt):
-                return redirect(url_for('index'))
+                login_user(user, remember=remember)
+                return redirect(url_for('search'))
 
     return render_template('login.html', form=form)
 
@@ -165,14 +193,20 @@ def signup():
 
         with conn.cursor() as cur:
             cur.execute(""" CREATE TABLE IF NOT EXISTS users(
-                name text, email text, pass text, PRIMARY KEY (name)) """)
-            query_sql = """ INSERT INTO users(name, email, pass) VALUES(%s, %s, %s) """
+                name text, email text, password text, PRIMARY KEY (name)) """)
+            query_sql = """ INSERT INTO users(name, email, password) VALUES(%s, %s, %s) """
             cur.execute(query_sql, (username, email, hashed_password,))
             conn.commit()
 
         return redirect(url_for('login'))
 
     return render_template('signup.html', form=form)
+
+@app.route("/logout", methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
